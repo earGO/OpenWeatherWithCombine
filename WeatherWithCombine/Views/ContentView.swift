24dce0c53c02f9206fc626a8680b6fbb7a9b0e07
earGO <rh.edwin@gmail.com>
2021-03-01 17:43:10 -0600

    first commit

---
+++ WeatherWithCombine/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherWithCombine
+//
+//  Created by Edwin Odesseiron on 3/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeatherWithCombine/WeatherWithCombineApp.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherWithCombineApp.swift
+//  WeatherWithCombine
+//
+//  Created by Edwin Odesseiron on 3/1/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherWithCombineApp: App {
+	let mainView = ContentView();
+    var body: some Scene {
+        WindowGroup {
+		mainView
+        }
+    }
+}
